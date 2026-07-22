667a387c4acddae05200d6c99fbf82207e2dbe70
Bohdan Tsaplia <me@gishadev.com>
2026-07-22 22:52:24 +0200

    first commit

---
+++ Assets/_Project/Scripts/Infrastructure/GameLifetimeScope.cs
@@ -0,0 +1,12 @@
+using gishadev.tools.Infrastructure;
+using VContainer;
+
+namespace gishadev.gmtk
+{
+    public class GameLifetimeScope : GishadevToolsLifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+        }
+    }
+}
