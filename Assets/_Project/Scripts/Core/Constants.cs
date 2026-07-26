e41c4da5cdfe928ff8eaff019fef11e23b28ddc4
Bohdan Tsaplia <me@gishadev.com>
2026-07-26 11:04:10 +0200

    add intro & fixes

--- Assets/_Project/Scripts/Core/Constants.cs
+++ Assets/_Project/Scripts/Core/Constants.cs
@@ -7,9 +7,9 @@ namespace gishadev.gmtk.Core
         public const string INTERACTABLE_LAYER_NAME = "Interactable";
         public const string MOVING_PLATFORM_TAG = "MovingPlatform";
         public const string FINAL_SCENE_NAME = "Final";
+        public static string GAME_SCENE_NAME = "Game";
         public static readonly int KID_STATE_ANIM = Animator.StringToHash("State");
         public static readonly int IS_RAISED_HAND_ANIM = Animator.StringToHash("IsRaisedHand");
         public static readonly int IS_COUNTING_ANIM = Animator.StringToHash("IsCounting");
-
     }
 }
---
+++ Assets/_Project/Scripts/Intro/IntroSequenceController.cs
@@ -0,0 +1,54 @@
+using gishadev.gmtk.Core;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+namespace gishadev.gmtk.Intro
+{
+    [RequireComponent(typeof(VideoPlayer))]
+    public class IntroSequenceController : MonoBehaviour
+    {
+        [SerializeField] private VideoPlayer videoPlayer;
+
+        private bool _transitioned;
+
+        private void Reset() => videoPlayer = GetComponent<VideoPlayer>();
+        private void OnEnable() => videoPlayer.loopPointReached += OnVideoFinished;
+        private void OnDisable() => videoPlayer.loopPointReached -= OnVideoFinished;
+        private void OnVideoFinished(VideoPlayer source) => GoToNextScene();
+
+        private void Update()
+        {
+            if (_transitioned)
+                return;
+
+            if (AnyButtonPressed())
+                GoToNextScene();
+        }
+
+        private static bool AnyButtonPressed()
+        {
+            var keyboard = Keyboard.current;
+            if (keyboard != null && keyboard.anyKey.wasPressedThisFrame)
+                return true;
+
+            var gamepad = Gamepad.current;
+            if (gamepad != null && gamepad.buttonSouth.wasPressedThisFrame)
+                return true;
+
+            return false;
+        }
+
+        private void GoToNextScene()
+        {
+            if (_transitioned)
+                return;
+
+            _transitioned = true;
+            videoPlayer.Stop();
+
+            SceneManager.LoadSceneAsync(Constants.GAME_SCENE_NAME);
+        }
+    }
+}
